900112524d6eeb4ce665822127350dc4ccdad4b9
Chris Scoggins <thescogg@gmail.com>
2017-02-22 19:59:10 -0500

    Major navbar rework

--- public/javascript/scripts.js
+++ public/javascript/scripts.js
@@ -12,9 +12,41 @@ $(document).ready(function (){
 
   // Toggle 'My Account' and 'Logout' options when clicking username in navbar
     $(".username").click(function () {
-      $(".userDropdown").toggle();
-    })
+      $(".userDropdown").slideToggle();
+    });
 
+  // Color navbar icons when hovering over navbar sections
+    $(".nav li a").hover(
+        function () {
+            // If class contains fa-home, fa-spinner, etc...
 
+            // Assign particular icon to variable
+            var $icon = ($(this).children());
+            console.log($icon);
 
+            if ($icon.hasClass("fa-home")){
+                $($icon).css("color", "cyan");
+            } else if ($icon.hasClass("fa-spinner")) {
+                $($icon).css("color", "magenta");
+            } else if ($icon.hasClass("fa-music")) {
+                $($icon).css("color", "yellow");
+            } else if ($icon.hasClass("fa-envelope")) {
+                $($icon).css("color", "orange");
+            }
+        },
+        function () {
+            // Assign particular icon to variable.
+            // Try to DRY this with a function.
+            var $icon = ($(this).children());
+            console.log($icon);
+
+            $icon.css("color", "white");
+        }
+    );
+
+    //Attempt to automatically assign colors to links in proper order
+    links = ($(".nav li").children());
+    for (link in links) {
+
+    }
 });
